179e4244cab3973af089cd1e835fe2a19164472c
Aditya Dhakal <dhakaladitya96@gmail.com>
2023-08-05 23:05:53 +0545

    ..

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,16 @@
+export default function UserProfile({ params }: any) {
+    return (
+
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="mb-5">profile</h1>
+            <hr />
+            <p className="text-4xl">profile
+                <span className="p-2 rounded bg-orange-500 text-black">{params.id}</span>
+            </p>
+        </div>
+
+    )
+
+
+
+}
